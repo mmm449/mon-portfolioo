a73944990ed9be0e6e1c1af43dc2db62b88ca71b
Magomed Maïgov <magomed.maigov2026@campus-eni.fr>
2026-04-04 00:10:08 +0200

    Correction

--- app/page.tsx
+++ app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
             </a>
             <a href="https://discord.com/users/lightt7565" target="_blank"
               className="flex items-center gap-3 px-8 py-4 bg-slate-50 text-slate-900 border border-slate-200 rounded-2xl font-bold hover:bg-slate-100 hover:scale-105 transition-all duration-300">
-              <img src="https://cdn3.emoji.gg/emojis/222991-discordlogo.gif" width="24px" height="24px" alt="discordlogo" />
+              <img src="https://cdn3.emoji.gg/emojis/222991-discordlogo.gif" width="64px" height="64px" alt="discordlogo" />
               <span> Mon Discord</span>
             </a>
           </div>
