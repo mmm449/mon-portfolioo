11581746ea5743176472ee03ecedddd8009c7565
Magomed Maïgov <magomed.maigov2026@campus-eni.fr>
2026-04-04 09:51:59 +0200

    Correction

--- app/page.tsx
+++ app/page.tsx
@@ -159,7 +159,7 @@ export default function Home() {
               <span className="flex items-center">Mon LinkedIn</span>
             </a>
             <a href="https://discord.com/users/lightt7565" target="_blank" rel="noreferrer"
-              className="flex items-center justify-center gap-3 w-50 h-14 bg-blue-600 text-white rounded-2xl font-bold shadow-xl shadow-blue-500/20 hover:bg-blue-700 hover:scale-105 transition-all duration-300 whitespace-nowrap">
+              className="flex items-center justify-center gap-3 w-60 h-14 bg-blue-600 text-white rounded-2xl font-bold shadow-xl shadow-blue-500/20 hover:bg-blue-700 hover:scale-105 transition-all duration-300 whitespace-nowrap">
               <img src="https://cdn3.emoji.gg/emojis/222991-discordlogo.gif"
                 className="w-6 h-6 object-contain inline-block"
                 alt="discordlogo" />
